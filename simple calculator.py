f7ced2c334bb09f5bdf38ba9ea9f17318b8f5b33
Vishwa0302 <61227291+Vishwa0302@users.noreply.github.com>
2020-10-01 08:14:24 +0530

    changes.md

--- simple calculator.py
+++ simple calculator.py
@@ -1,161 +1,312 @@
 # Python program to  create a simple GUI  
 # calculator using Tkinter 
   
-# import everything from tkinter module 
 from tkinter import *
-  
-# globally declare the expression variable 
-expression = "" 
-  
-  
-# Function to update expressiom 
-# in the text entry box 
-def press(num): 
-    # point out the global expression variable 
-    global expression 
-  
-    # concatenation of string 
-    expression = expression + str(num) 
-  
-    # update the expression by using set method 
-    equation.set(expression) 
-  
-  
-# Function to evaluate the final expression 
-def equalpress(): 
-    # Try and except statement is used 
-    # for handling the errors like zero 
-    # division error etc. 
-  
-    # Put that code inside the try block 
-    # which may generate the error 
-    try: 
-  
-        global expression 
-  
-        # eval function evaluate the expression 
-        # and str function convert the result 
-        # into string 
-        total = str(eval(expression)) 
-  
-        equation.set(total) 
-  
-        # initialze the expression variable 
-        # by empty string 
-        expression = "" 
-  
-    # if error is generate then handle 
-    # by the except block 
-    except: 
-  
-        equation.set(" error ") 
-        expression = "" 
-  
-  
-# Function to clear the contents 
-# of text entry box 
-def clear(): 
-    global expression 
-    expression = "" 
-    equation.set("") 
-  
-  
-# Driver code 
-if __name__ == "__main__": 
-    # create a GUI window 
-    gui = Tk() 
-  
-    # set the background colour of GUI window 
-    gui.configure(background="light green") 
-  
-    # set the title of GUI window 
-    gui.title("Simple Calculator") 
-  
-    # set the configuration of GUI window 
-    gui.geometry("265x125") 
-  
-    # StringVar() is the variable class 
-    # we create an instance of this class 
-    equation = StringVar() 
-  
-    # create the text entry box for 
-    # showing the expression . 
-    expression_field = Entry(gui, textvariable=equation) 
-  
-    # grid method is used for placing 
-    # the widgets at respective positions 
-    # in table like structure . 
-    expression_field.grid(columnspan=4, ipadx=70) 
-  
-     
-  
-    # create a Buttons and place at a particular 
-    # location inside the root window . 
-    # when user press the button, the command or 
-    # function affiliated to that button is executed . 
-    button1 = Button(gui, text=' 1 ', fg='black', bg='red', 
-                     command=lambda: press(1), height=1, width=7) 
-    button1.grid(row=2, column=0) 
-  
-    button2 = Button(gui, text=' 2 ', fg='black', bg='red', 
-                     command=lambda: press(2), height=1, width=7) 
-    button2.grid(row=2, column=1) 
-  
-    button3 = Button(gui, text=' 3 ', fg='black', bg='red', 
-                     command=lambda: press(3), height=1, width=7) 
-    button3.grid(row=2, column=2) 
-  
-    button4 = Button(gui, text=' 4 ', fg='black', bg='red', 
-                     command=lambda: press(4), height=1, width=7) 
-    button4.grid(row=3, column=0) 
-  
-    button5 = Button(gui, text=' 5 ', fg='black', bg='red', 
-                     command=lambda: press(5), height=1, width=7) 
-    button5.grid(row=3, column=1) 
-  
-    button6 = Button(gui, text=' 6 ', fg='black', bg='red', 
-                     command=lambda: press(6), height=1, width=7) 
-    button6.grid(row=3, column=2) 
-  
-    button7 = Button(gui, text=' 7 ', fg='black', bg='red', 
-                     command=lambda: press(7), height=1, width=7) 
-    button7.grid(row=4, column=0) 
-  
-    button8 = Button(gui, text=' 8 ', fg='black', bg='red', 
-                     command=lambda: press(8), height=1, width=7) 
-    button8.grid(row=4, column=1) 
-  
-    button9 = Button(gui, text=' 9 ', fg='black', bg='red', 
-                     command=lambda: press(9), height=1, width=7) 
-    button9.grid(row=4, column=2) 
-  
-    button0 = Button(gui, text=' 0 ', fg='black', bg='red', 
-                     command=lambda: press(0), height=1, width=7) 
-    button0.grid(row=5, column=0) 
-  
-    plus = Button(gui, text=' + ', fg='black', bg='red', 
-                  command=lambda: press("+"), height=1, width=7) 
-    plus.grid(row=2, column=3) 
-  
-    minus = Button(gui, text=' - ', fg='black', bg='red', 
-                   command=lambda: press("-"), height=1, width=7) 
-    minus.grid(row=3, column=3) 
-  
-    multiply = Button(gui, text=' * ', fg='black', bg='red', 
-                      command=lambda: press("*"), height=1, width=7) 
-    multiply.grid(row=4, column=3) 
-  
-    divide = Button(gui, text=' / ', fg='black', bg='red', 
-                    command=lambda: press("/"), height=1, width=7) 
-    divide.grid(row=5, column=3) 
-  
-    equal = Button(gui, text=' = ', fg='black', bg='red', 
-                   command=equalpress, height=1, width=7) 
-    equal.grid(row=5, column=2) 
-  
-    clear = Button(gui, text='Clear', fg='black', bg='red', 
-                   command=clear, height=1, width=7) 
-    clear.grid(row=5, column='1') 
-  
-    # start the GUI 
-    gui.mainloop() 
+from math import *
+
+root = Tk()
+root.title("Calculator")
+root.resizable(width=False, height=False)
+
+screen = StringVar()
+screen.set("0")
+
+current = ""
+power = ""
+
+firstnum = str()
+secondnum = str()
+mathsign = str()
+
+defxworking = False
+percentt = False
+
+def math_button_pressed():
+    if mathsign == '+':
+        button_plus.config(relief=SUNKEN)
+    if mathsign == '-':
+        button_minus.config(relief=SUNKEN)
+    if mathsign == '*':
+        button_multiply.config(relief=SUNKEN)
+    if mathsign == '/':
+        button_division.config(relief=SUNKEN)
+
+def math_button_raised():
+    button_plus.config(relief=RAISED)
+    button_minus.config(relief=RAISED)
+    button_multiply.config(relief=RAISED)
+    button_division.config(relief=RAISED)
+
+def is_int(num):
+    if int(num) == float(num):
+        return int(num)
+    else:
+        return float(num)
+
+def number_pressed(butt):
+    global current, power, firstnum, secondnum
+
+    if mathsign == str() and defxworking == False:
+        current = current + str(butt)
+        screen.set(current)
+        firstnum = float(current)
+
+    elif mathsign != str() and defxworking == False:
+        math_button_raised()
+        current = current + str(butt)
+        screen.set(current)
+        secondnum = float(current)
+
+    elif mathsign == str() and defxworking == True:
+        power = power + str(butt)
+        current = current + str(butt)
+        screen.set(current)
+
+    elif mathsign != str and defxworking == True:
+        power = power + str(butt)
+        current = current + str(butt)
+        screen.set(current)
+        print(power)
+
+def math_pressed(math):
+    global current, power, mathsign, firstnum, secondnum, defxworking, percentt
+
+    if mathsign == str() and defxworking == False and percentt == False and firstnum != str():
+        mathsign = str(math)
+        math_button_pressed()
+        current = ""
+
+    elif mathsign != str() and defxworking == False and percentt == False:
+        print(2)
+        if mathsign == '+':
+            firstnum = round(float(firstnum + secondnum),6)
+        if mathsign == '-':
+            firstnum = round(float(firstnum - secondnum),6)
+        if mathsign == '*':
+            firstnum = round(float(firstnum * secondnum),6)
+        if mathsign == '/':
+            firstnum = round(float(firstnum / secondnum),6)
+        screen.set(is_int(firstnum))
+
+        mathsign = str(math)
+        math_button_pressed()
+        current = ""
+
+    elif mathsign != str() and defxworking == True and percentt == False:
+        if mathsign == '+':
+            firstnum = round(firstnum + secondnum ** int(power),6)
+        if mathsign == '-':
+            firstnum = round(firstnum - secondnum ** int(power),6)
+        if mathsign == '*':
+            firstnum = round(firstnum * (secondnum ** int(power)),6)
+        if mathsign == '/':
+            firstnum = round(firstnum / (secondnum ** int(power)),6)
+        defxworking = False
+        screen.set(is_int(firstnum))
+        defxworking = False
+        mathsign = str(math)
+        math_button_pressed()
+        power = ""
+        current = ""
+
+    elif defxworking and percentt == False:
+        firstnum = round(firstnum ** int(power), 6)
+        defxworking = False
+        screen.set(is_int(firstnum))
+        mathsign = str(math)
+        math_button_pressed()
+        power = ""
+        current = ""
+
+    elif percentt:
+        if mathsign == '+':
+            firstnum = round(float(firstnum + firstnum/100*secondnum),6)
+        if mathsign == '-':
+            firstnum = round(float(firstnum - firstnum/100*secondnum),6)
+        screen.set(is_int(firstnum))
+        percentt = False
+        mathsign = str(math)
+        math_button_pressed()
+        current = ""
+
+def squareroot():
+    global firstnum, secondnum, mathsign, current
+
+    if mathsign == str():
+        firstnum = round(sqrt(firstnum),6)
+        screen.set(is_int(firstnum))
+
+    if mathsign != str():
+        if mathsign == '+':
+            firstnum = round(sqrt(firstnum + float(secondnum)),6)
+        if mathsign == '-':
+            firstnum = round(sqrt(firstnum - float(secondnum)),6)
+        if mathsign == '*':
+            firstnum = round(sqrt(firstnum * float(secondnum)),6)
+        if mathsign == '/':
+            firstnum = round(sqrt(firstnum / float(secondnum)),6)
+
+        screen.set(is_int(firstnum))
+        secondnum = str()
+        mathsign = str()
+        current = ""
+
+def x():
+    global firstnum, secondnum, mathsign, current, defxworking
+
+    if mathsign == str():
+        current = str(is_int(firstnum)) + '^'
+        screen.set(current)
+        defxworking = True
+
+    elif mathsign != str():
+
+        current = str(is_int(secondnum)) + '^'
+        screen.set(current)
+        defxworking = True
+
+def result():
+    global firstnum, secondnum, mathsign, current, power, defxworking, percentt
+    if defxworking == False and percentt == False:
+        if mathsign == '+':
+            firstnum = round(float(firstnum + secondnum),6)
+        if mathsign == '-':
+            firstnum = round(float(firstnum - secondnum),6)
+        if mathsign == '*':
+            firstnum = round(float(firstnum * secondnum),6)
+        if mathsign == '/':
+            firstnum = round(float(firstnum / secondnum),6)
+        screen.set(is_int(firstnum))
+
+    if mathsign == str() and defxworking == True and percentt == False:
+        firstnum = round(firstnum ** int(power),6)
+        defxworking = False
+        screen.set(is_int(firstnum))
+
+    if mathsign != str() and defxworking == True and percentt == False:
+        if mathsign == '+':
+            firstnum = round(firstnum + secondnum ** int(power),6)
+            defxworking = False
+        if mathsign == '-':
+            firstnum = round(firstnum - secondnum ** int(power),6)
+            defxworking = False
+        if mathsign == '*':
+            firstnum = round(firstnum * (secondnum ** int(power)),6)
+            defxworking = False
+        if mathsign == '/':
+            firstnum = round(firstnum / (secondnum ** int(power)),6)
+            defxworking = False
+        screen.set(is_int(firstnum))
+
+
+    if defxworking == False and percentt == True:
+        if mathsign == '+':
+            firstnum = round(float(firstnum + firstnum/100*secondnum),6)
+            screen.set(is_int(firstnum))
+            percentt = False
+        if mathsign == '-':
+            firstnum = round(float(firstnum - firstnum/100*secondnum),6)
+            screen.set(is_int(firstnum))
+            percentt = False
+
+    mathsign = str()
+    current = ""
+    power = ""
+
+    if defxworking == False and mathsign == '*' or '/' and percentt == True:
+        clear()
+
+def clear():
+    global current, firstnum, secondnum, mathsign, power, defxworking, percentt
+
+    screen.set(0)
+    current = ""
+    power = ""
+    firstnum = str()
+    secondnum = str()
+    mathsign = str()
+    defxworking = False
+    math_button_raised()
+    percentt = False
+
+def percent():
+    global firstnum, secondnum, current, percentt
+
+    current = str(is_int(secondnum)) + '%'
+    screen.set(current)
+    percentt = True
+
+
+
+# Widgets
+
+calculation = Entry(root, textvariable = screen, font=("Verdana", 15, ), bd = 12,
+                    insertwidth=4, width=14, justify=RIGHT)
+calculation.grid(columnspan=4)
+#   Numbers
+button1 = Button(root, text='1', command=lambda: number_pressed(1), bg="gainsboro",
+                 bd=3, padx=12, pady=5, font=("Helvetica", 14, "bold"))
+button1.grid(row=2, column=0, sticky=W)
+button2 = Button(root, text='2', command=lambda: number_pressed(2), bg="gainsboro",
+                 bd=3, padx=12, pady=5, font=("Helvetica", 14, "bold"))
+button2.grid(row=2, column=1, sticky=W)
+button3 = Button(root, text='3', command=lambda: number_pressed(3), bg="gainsboro",
+                 bd=3, padx=12, pady=5, font=("Helvetica", 14, "bold"))
+button3.grid(row=2, column=2, sticky=W)
+button4 = Button(root, text='4', command=lambda: number_pressed(4), bg="gainsboro",
+                 bd=3, padx=12, pady=5, font=("Helvetica", 14, "bold"))
+button4.grid(row=3, column=0, sticky=W)
+button5 = Button(root, text='5', command=lambda: number_pressed(5), bg="gainsboro",
+                 bd=3, padx=12, pady=5, font=("Helvetica", 14, "bold"))
+button5.grid(row=3, column=1, sticky=W)
+button6 = Button(root, text='6', command=lambda: number_pressed(6), bg="gainsboro",
+                 bd=3, padx=12, pady=5, font=("Helvetica", 14, "bold"))
+button6.grid(row=3, column=2, sticky=W)
+button7 = Button(root, text='7', command=lambda: number_pressed(7), bg="gainsboro",
+                 bd=3, padx=12, pady=5, font=("Helvetica", 14, "bold"))
+button7.grid(row=4, column=0, sticky=W)
+button8 = Button(root, text='8', command=lambda: number_pressed(8), bg="gainsboro",
+                 bd=3, padx=12, pady=5, font=("Helvetica", 14, "bold"))
+button8.grid(row=4, column=1, sticky=W)
+button9 = Button(root, text='9', command=lambda: number_pressed(9), bg="gainsboro",
+                 bd=3, padx=12, pady=5, font=("Helvetica", 14, "bold"))
+button9.grid(row=4, column=2, sticky=W)
+button0 = Button(root, text='0', command=lambda: number_pressed(0), bg="gainsboro",
+                 bd=3, padx=12, pady=5, font=("Helvetica", 14, "bold"))
+button0.grid(row=5, column=0, sticky=W)
+button_float = Button(root, text='.', command=lambda: number_pressed('.'), bg="gainsboro",
+                      bd=3, padx=15, pady=5, font=("Helvetica", 14, "bold"))
+button_float.grid(row=5, column=1)
+
+#   Math signs
+button_plus = Button(root, text='+', command=lambda: math_pressed('+'), bg="gray70",
+                     bd=3, padx=11, pady=5, font=("Helvetica", 14, "bold"))
+button_plus.grid(row=2, column=3, sticky=W)
+button_minus = Button(root, text='-', command=lambda: math_pressed('-'),  bg="gray70",
+                      bd=3, padx=11, pady=4, font=("Verdana", 14, "bold"))
+button_minus.grid(row=3, column=3, sticky=W)
+button_multiply = Button(root, text='*', command=lambda: math_pressed('*'), bg="gray70",
+                         bd=3, padx=13, pady=5, font=("Helvetica", 14, "bold"))
+button_multiply.grid(row=4, column=3, )
+button_division = Button(root, text='/', command=lambda: math_pressed('/'),  bg="gray70",
+                         bd=3, padx=14, pady=5, font=("Helvetica", 14, "bold"))
+button_division.grid(row=5, column=3, )
+button_equal = Button(root, text='=', command=lambda: result(), bg='orange',
+                      bd=3, padx=12, pady=5, font=("Arial", 14))
+button_equal.grid(row=5, column=2, )
+
+button_percent = Button(root, text='%', command=lambda: percent(),  bg="gray70",
+                         bd=3, padx=8, pady=5, font=("Helvetica", 14, "bold"))
+button_percent.grid(row=1, column=3, )
+
+button_clear = Button(root, text='C', command=lambda: clear(), bg='gray70',
+                      bd=3, padx=11, pady=5, font=("Helvetica", 14))
+button_clear.grid(row=1, column=0)
+button_sqrt = Button(root, text='√', command=lambda: squareroot(), bg="gray70",
+                        bd=3, padx=12, pady=5, font=("Helvetica", 14, "bold"))
+button_sqrt.grid(row=1, column=1, sticky=W)
+button_x = Button(root, text='x^y', command=lambda: x(), bg="gray70",
+                  bd=3, padx=6, pady=5, font=("Helvetica", 14))
+button_x.grid(row=1, column=2, sticky=W)
+
+root.mainloop()
